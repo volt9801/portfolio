5002854c7b2156f4e19c96957c63e1d4ad1635d0
volt9801 <pranshuj73@gmail.com>
2020-05-24 20:15:17 +0530

    Updates

---
+++ precache-manifest.a6e42f820781b893780e5de1d348ee21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11af95218ecf08aa22f488361b2e6464",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ed2c89ad2a6459c716ec",
+    "url": "/portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4639aba5aea04611ec27",
+    "url": "/portfolio/static/css/main.2df5c932.chunk.css"
+  },
+  {
+    "revision": "ed2c89ad2a6459c716ec",
+    "url": "/portfolio/static/js/2.3651e577.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.3651e577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4639aba5aea04611ec27",
+    "url": "/portfolio/static/js/main.4b6ed08b.chunk.js"
+  },
+  {
+    "revision": "26c48c9fe455ff3fbd94",
+    "url": "/portfolio/static/js/runtime-main.37353901.js"
+  }
+]);
