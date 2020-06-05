350ef75fc23850596138763a88a07dbbefe536fb
volt9801 <pranshuj73@gmail.com>
2020-06-05 22:58:23 +0530

    still fixing

--- pranshus-portfolio/src/components/introduction.jsx
+++ pranshus-portfolio/src/components/introduction.jsx
@@ -7,7 +7,7 @@ export default class Introduction extends Component {
 					<div className="order-md-2 px-auto">
 						<img src="pj.svg" alt="Pranshu Jha" style={{ borderRadius: '50%', width: '200px', height: '200px', objectFit: 'cover', border: '5px solid #fff' }}/>
 					</div>
-					<div className="order-md-1 px-auto">
+					<div className="intro-text order-md-1 px-auto">
 						<span style={{ fontSize: 25, fontFamily: 'Raleway', fontWeight: 'bold', letterSpacing: '5px' }}>Hi! I am</span>
 						<br />
 						<span className="links" style={{ fontSize: 50, fontFamily: 'Raleway', fontWeight: 'bold', letterSpacing: '5px', borderBottom: '5px solid #fff' }}>Pranshu Jha</span>
