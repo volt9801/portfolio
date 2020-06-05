02fe521ecf45cfbd92080aaca4c2d390441dc150
volt9801 <pranshuj73@gmail.com>
2020-06-05 23:02:08 +0530

    fixing...

--- pranshus-portfolio/src/components/introduction.jsx
+++ pranshus-portfolio/src/components/introduction.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Introduction extends Component {
 	render() {
 		return (
-			<div class="overlay d-flex align-items-center justify-content-center flex-wrap py-5">
+			<div class="overlay d-flex align-items-center justify-content-between flex-wrap py-5">
 					<div className="order-md-2 px-auto">
 						<img src="pj.svg" alt="Pranshu Jha" style={{ borderRadius: '50%', width: '200px', height: '200px', objectFit: 'cover', border: '5px solid #fff' }}/>
 					</div>
