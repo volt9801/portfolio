12a26f268a51ec949bcf3aa07ed04fa6e6626b47
volt9801 <pranshuj73@gmail.com>
2020-06-05 21:42:47 +0530

    changed profile picture

--- pranshus-portfolio/src/components/introduction.jsx
+++ pranshus-portfolio/src/components/introduction.jsx
@@ -8,9 +8,9 @@ export default class Introduction extends Component {
 						<img src="pj.svg" alt="Pranshu Jha" style={{ borderRadius: '50%', width: '250px', height: '250px', objectFit: 'cover' }}/>
 					</div>
 					<div className="order-md-1 px-5">
-						<span style={{ fontSize: 30, fontFamily: 'Raleway', fontWeight: 'bold', letterSpacing: '5px' }}>Hi! I am</span>
+						<span style={{ fontSize: 25, fontFamily: 'Raleway', fontWeight: 'bold', letterSpacing: '5px' }}>Hi! I am</span>
 						<br />
-						<span className="links" style={{ fontSize: 50, fontFamily: 'Raleway', fontWeight: 'bold', letterSpacing: '5px', borderBottom: '5px solid #fff' }}>Pranshu Jha</span>
+						<span className="links" style={{ fontSize: 45, fontFamily: 'Raleway', fontWeight: 'bold', letterSpacing: '5px', borderBottom: '5px solid #fff' }}>Pranshu Jha</span>
 					</div>
 			</div>
 		)
--- pranshus-portfolio/src/components/projects.jsx
+++ pranshus-portfolio/src/components/projects.jsx
@@ -6,8 +6,8 @@ export default class Projects extends Component {
 			<div className="container mt-5 pt-5 pb-5" data-aos="fade" data-aos-easing="ease-in-out" data-aos-duration="750">
 				<h2 className="text-center font-weight-bold pb-3" style={{ textDecoration: 'underline', textUnderlinePosition: 'under', letterSpacing: '2px' }}>Projects</h2>
 				<div className="container row row-col-1">
-					<div className="card m-2 shadow" style={{ minWidth: '15rem' }} data-aos="fade" data-aos-easing="ease-in-out" data-aos-duration="750">
-						<div className="p-2 pt-3">
+					<div className="card m-2 shadow" style={{ minWidth: '15rem', borderRadius: '10px' }} data-aos="fade" data-aos-easing="ease-in-out" data-aos-duration="750">
+						<div className="p-3">
 						  <h5 className="card-title p-1"><i class="fab fa-github fa-lg"></i> N/A</h5>
 						  <p className="pt-3 card-text lead gray-text">
 						  	N/A
@@ -15,7 +15,7 @@ export default class Projects extends Component {
 						</div>
 					</div>
 				</div>
-				<p className="pt-5 card-text lead gray-text pb-5">
+				<p className="pt-5 card-text lead gray-text pb-5 text-center">
 					See more of my projects on <a className="links p-1" style={{ color: '#696969' }} href="https://github.com/volt9801"><i class="fab fa-github fa-lg"></i> GitHub</a>
 				</p>
 			</div>
